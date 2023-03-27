6e9190e15b5f08df38cee9655f097a67c266dc17
bgashler1 <bgashler1@gmail.com>
2023-03-27 16:13:25 -0700

    Switching to async/await for extension code (readability)

--- src/extension.ts
+++ src/extension.ts
@@ -13,7 +13,7 @@ function getTabString(editor: vscode.TextEditor): string {
 export function activate(extensionContext?: vscode.ExtensionContext) {
 
 	// The command's name is pre-declared in package.json
-	vscode.commands.registerCommand('extension.htmlTagWrap', () => {
+	vscode.commands.registerCommand('extension.htmlTagWrap', async () => {
 		// Code in this function runs each time extension is activated
 
 		const editor = vscode.window.activeTextEditor;
@@ -44,13 +44,13 @@ export function activate(extensionContext?: vscode.ExtensionContext) {
 		
 		let tag = vscode.workspace.getConfiguration().get<string>("htmltagwrap.tag");
 		const autoDeselectClosingTag = vscode.workspace.getConfiguration().get<boolean>("htmltagwrap.autoDeselectClosingTag");
-		
+
 		if (!tag) {
 			tag = 'p'; 
 		}
 
 		// Start inserting tags
-		editor.edit((editBuilder) => {
+		await editor.edit((editBuilder) => {
 
 			const selections = editor.selections;
 			
@@ -95,17 +95,17 @@ export function activate(extensionContext?: vscode.ExtensionContext) {
 		}, {
 			undoStopBefore: true,
 			undoStopAfter: false
-		}).then(() => {
-			// Add tag name elements
-
-			// Need to fetch selections again as they are no longer accurate
-			const selections = editor.selections;
-			return editor.edit((editBuilder) => {
-
+		});
+		// Add tag name elements
+		// Need to fetch selections again as they are no longer accurate
+		let selections = editor.selections;
+		try {
+			await editor.edit((editBuilder) => {
+	
 				const tagsMissingElementsSelections: vscode.Selection[] = tagsMissingElements.map(index => {
 					return selections[index];
 				});
-
+	
 				tagsMissingElementsSelections.map(selection => {
 					let tagFirst = selection.start.translate(0,-1);
 					const tagSecond = selection.end.translate(0,-1);
@@ -118,171 +118,165 @@ export function activate(extensionContext?: vscode.ExtensionContext) {
 					editBuilder.insert(tagFirst, tag);
 					editBuilder.insert(tagSecond, tag);
 				});
+				throw new Error ('BLAHHH!!!');
 			}, {
 				undoStopBefore: false,
 				undoStopAfter: true
 			});
-		}, (err) => {
-			console.log('Element name insertion rejected!');
-			console.error(err);
-		}).then(() => {
 			console.log('Edit applied!');
-			const selections = editor.selections;
-			const toSelect: Array<vscode.Selection> = new Array<vscode.Selection>();
-			return new Promise(resolve => {
-				// Need to fetch selections again as they are no longer accurate
+		}
+		catch (err) {
+			console.error('Element name insertion rejected! ', err);
+		}
+			
+		// Need latest prior selections again or we'll update them incorrectly
+		selections = editor.selections;
+		const toSelect: Array<vscode.Selection> = new Array<vscode.Selection>();
+
+		await new Promise(resolve => {
+			// Need to fetch selections again as they are no longer accurate
+			for(const selection of selections) {
 				
+				// Careful : the selection starts at the beginning of the text but ends *after* the closing tag
+				if(selection.end.line !== selection.start.line) {
+					// ================
+					// Block selection
+					// ================
+					const lineAbove = selection.start.line - 1;
+					const lineBelow = selection.end.line;
+					const startPosition = selection.start.character - tabSizeSpace.length + 1;
+					const endPosition = selection.end.character - 1 - tag.length;
 
-				for(const selection of selections) {
-					
-					// Careful : the selection starts at the beginning of the text but ends *after* the closing tag
-					if(selection.end.line !== selection.start.line) {
-						// ================
-						// Block selection
-						// ================
-						const lineAbove = selection.start.line - 1;
-						const lineBelow = selection.end.line;
-						const startPosition = selection.start.character - tabSizeSpace.length + 1;
-						const endPosition = selection.end.character - 1 - tag.length;
-
-						toSelect.push(new vscode.Selection(lineAbove, startPosition, lineAbove, startPosition + tag.length));
-						toSelect.push(new vscode.Selection(lineBelow, endPosition, lineBelow, endPosition + tag.length));
-					}
-					else {
-						// ================
-						// Inline selection
-						// ================
-						// same line, just get to the tag element by navigating backwards
-						let startPosition = selection.start.character - 1 - tag.length;
-						const endPosition = selection.end.character - 1 - tag.length;
-
-						if(selection.start.character === selection.end.character) {
-							// Empty selection
-							startPosition = startPosition - 3 - tag.length;
-						}
-
-						toSelect.push(new vscode.Selection(selection.start.line, startPosition, selection.start.line, startPosition + tag.length));
-						toSelect.push(new vscode.Selection(selection.end.line, endPosition, selection.end.line, endPosition + tag.length));
-					}
-					resolve(null);
-					
+					toSelect.push(new vscode.Selection(lineAbove, startPosition, lineAbove, startPosition + tag.length));
+					toSelect.push(new vscode.Selection(lineBelow, endPosition, lineBelow, endPosition + tag.length));
 				}
-			}).then(() => {
-				return new Promise(resolve => {
-					editor.selections = toSelect;
-					const windowListener = vscode.window.onDidChangeTextEditorSelection((event)=> {
-						resolve('✔ Selections updated');
-					});
-				});
-			}).then(selectionsPromiseFulfilled => {
-				console.log(selectionsPromiseFulfilled);
-				interface SpaceInsertedPromiseResolution {
-					spaceInsertedAt: vscode.Range;
-					initialSelections: readonly vscode.Selection[];
+				else {
+					// ================
+					// Inline selection
+					// ================
+					// same line, just get to the tag element by navigating backwards
+					let startPosition = selection.start.character - 1 - tag.length;
+					const endPosition = selection.end.character - 1 - tag.length;
+
+					if(selection.start.character === selection.end.character) {
+						// Empty selection
+						startPosition = startPosition - 3 - tag.length;
+					}
+
+					toSelect.push(new vscode.Selection(selection.start.line, startPosition, selection.start.line, startPosition + tag.length));
+					toSelect.push(new vscode.Selection(selection.end.line, endPosition, selection.end.line, endPosition + tag.length));
 				}
+				resolve(null);
 				
-				if (!autoDeselectClosingTag) {
-					return;
-				}
-				// Wait for selections to be made, then listen for changes.
-				// Enter a mode to listen for whitespace and remove the second cursor
-				let workspaceListener: vscode.Disposable;
-				let windowListener: vscode.Disposable;
-				const autoDeselectClosingTagAction = new Promise((resolve, reject) => {
-					
-					// Have selections changed?
-					const initialSelections = editor.selections;			
-					windowListener = vscode.window.onDidChangeTextEditorSelection((event)=> {
-						if (event.kind !== undefined && event.kind !== 1) {
-							// Listen for anything that changes selection but keyboard input
-							// or an undefined event (such as backspace clearing last selected character)
-							resolve('✔ User changed selection. Event type: ' + event.kind);
-						}
-					});
-					
-					// Did user enter a space
-					workspaceListener = vscode.workspace.onDidChangeTextDocument((event)=> {
-						const contentChange = event.contentChanges;
-						if (contentChange[0].text === ' ') {
-							// If the user presses space without typing anything, we need to resolve with a parameter and make sure to add back the tag names that were overwritten with a space
-							const resolution: SpaceInsertedPromiseResolution = {
-								spaceInsertedAt: contentChange[1].range,
-								initialSelections: initialSelections
-							};
-							resolve(resolution);
-						}
-					});
-				});
-				return autoDeselectClosingTagAction.then((success: SpaceInsertedPromiseResolution) => {
-					//Cleanup memory and processes
-					workspaceListener.dispose();
-					windowListener.dispose();
-					
-					const newSelections = new Array<vscode.Selection>();
-					const spacePressedWithoutTypingNewTag = ():boolean => {
-						if (success.spaceInsertedAt) {
-							const initialSelections = success.initialSelections;
-							const spaceInsertedAt = success.spaceInsertedAt;
-
-							// Selections array is in the order user made selections (arbitrary), whereas the spaceInsertedAt (content-edit array) is in logical order, so we must loop to compare.
-							let returnValue: boolean;
-							for (let i = 0; i < initialSelections.length; i++) {
-								if (spaceInsertedAt.isEqual(initialSelections[i])) {
-									returnValue = true;
-									//console.log('Selection[' + i + '] equal??? ' + returnValue);
-									break;
-								} else {
-									returnValue = false;
-								}
-							}
-							return returnValue;
-						}
-						else {
-							return false;
-						}
-					};
-					editor.edit((editBuilder) => {
-						// Update selections
-						const initialSelections = editor.selections;
-						const newLine = false;
-						const charOffset = 0;
-						const addMissingTag: boolean = spacePressedWithoutTypingNewTag();
-						for (const [index, selection] of initialSelections.entries()) {
-							let tagPosition: vscode.Position = selection.start;
-							if (index % 2 !== 0) {
-								// Remove whitespace on closing tag
-								// Since closing tag selection is now length zero and after the whitespace, select a range one character backwards
-								const closingTagWhitespace: vscode.Range = selection.with({start: selection.end.translate(0,-1), end: undefined});
-								editBuilder.delete(closingTagWhitespace);
-							} else {
-								tagPosition = selection.start.translate(undefined, -1);
-							}
-							if (addMissingTag) {
-								// If the user pressed space and overwrote the default tag with no tag, add the default tag before the space
-								editBuilder.insert(tagPosition, tag);
-							}
-						}
-					}, {
-						undoStopBefore: false,
-						undoStopAfter: false
-					}).then(()=> {
-						// Update selections
-						const initialSelections = editor.selections;
-						for (const [index, selection] of initialSelections.entries()) {
-							if (index % 2 === 0) {
-								newSelections.push(selection);
-							}
-						}
-					}).then(()=> {
-						editor.selections = newSelections;
-						console.log('✔︎ Deselected closing tags');
-					});
-				});
-
-			});
-		}, (err) => {
-			console.log('Edit rejected!');
-			console.error(err);
+			}
 		});
+		const selectionsPromiseFulfilled = await new Promise(resolve => {
+			editor.selections = toSelect;
+			vscode.window.onDidChangeTextEditorSelection((event)=> {
+				resolve('✔ Selections updated');
+			});
+		});
+		console.log(selectionsPromiseFulfilled);
+		try {
+			interface SpaceInsertedPromiseResolution {
+				spaceInsertedAt: vscode.Range;
+				initialSelections: readonly vscode.Selection[];
+			}
+			
+			if (!autoDeselectClosingTag) {
+				return;
+			}
+			// Wait for selections to be made, then listen for changes.
+			// Enter a mode to listen for whitespace and remove the second cursor
+			let workspaceListener: vscode.Disposable;
+			let windowListener: vscode.Disposable;
+			const autoDeselectClosingTagAction: Thenable<SpaceInsertedPromiseResolution | string> = new Promise((resolve, reject) => {
+				
+				// Have selections changed?
+				const initialSelections = editor.selections;			
+				windowListener = vscode.window.onDidChangeTextEditorSelection((event)=> {
+					if (event.kind !== undefined && event.kind !== 1) {
+						// Listen for anything that changes selection but keyboard input
+						// or an undefined event (such as backspace clearing last selected character)
+						resolve('✔ User changed selection. Event type: ' + event.kind);
+					}
+				});
+				
+				// Did user enter a space
+				workspaceListener = vscode.workspace.onDidChangeTextDocument((event)=> {
+					const contentChange = event.contentChanges;
+					if (contentChange[0].text === ' ') {
+						// If the user presses space without typing anything, we need to resolve with a parameter and make sure to add back the tag names that were overwritten with a space
+						const resolution: SpaceInsertedPromiseResolution = {
+							spaceInsertedAt: contentChange[1].range,
+							initialSelections: initialSelections
+						};
+						resolve(resolution);
+					}
+				});
+			});
+			const success = await autoDeselectClosingTagAction;
+				//Cleanup memory and processes
+				windowListener.dispose();
+				workspaceListener.dispose();
+				console.warn('typeof success = ');
+				
+				const newSelections = new Array<vscode.Selection>();
+				const spacePressedWithoutTypingNewTag = ():boolean => {
+					if (typeof success === 'string') return false;
+					const initialSelections = success.initialSelections;
+					const spaceInsertedAt = success.spaceInsertedAt;
+
+					// Selections array is in the order user made selections (arbitrary), whereas the spaceInsertedAt (content-edit array) is in logical order, so we must loop to compare.
+					let returnValue: boolean;
+					for (let i = 0; i < initialSelections.length; i++) {
+						if (spaceInsertedAt.isEqual(initialSelections[i])) {
+							returnValue = true;
+							//console.log('Selection[' + i + '] equal??? ' + returnValue);
+							break;
+						} else {
+							returnValue = false;
+						}
+					}
+					return returnValue;
+				};
+				await editor.edit((editBuilder) => {
+					// Update selections
+					const initialSelections = editor.selections;
+					const newLine = false;
+					const charOffset = 0;
+					const addMissingTag: boolean = spacePressedWithoutTypingNewTag();
+					for (const [index, selection] of initialSelections.entries()) {
+						let tagPosition: vscode.Position = selection.start;
+						if (index % 2 !== 0) {
+							// Remove whitespace on closing tag
+							// Since closing tag selection is now length zero and after the whitespace, select a range one character backwards
+							const closingTagWhitespace: vscode.Range = selection.with({start: selection.end.translate(0,-1), end: undefined});
+							editBuilder.delete(closingTagWhitespace);
+						} else {
+							tagPosition = selection.start.translate(undefined, -1);
+						}
+						if (addMissingTag) {
+							// If the user pressed space and overwrote the default tag with no tag, add the default tag before the space
+							editBuilder.insert(tagPosition, tag);
+						}
+					}
+				}, {
+					undoStopBefore: false,
+					undoStopAfter: false
+				});
+				// Update selections
+				const initialSelections = editor.selections;
+				for (const [index, selection] of initialSelections.entries()) {
+					if (index % 2 === 0) {
+						newSelections.push(selection);
+					}
+				}
+				editor.selections = newSelections;
+				console.log('✔︎ Deselected closing tags');
+		}
+		catch (err) {
+			console.error('Edit rejected!' + err);
+		}
 	});
 }
